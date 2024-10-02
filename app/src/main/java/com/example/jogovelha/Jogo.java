ce18d47b85f8e9ae32f5214765e2937b33667403
Moises Olimpio <moisesolimpio99@gmail.com>
2024-10-02 20:46:45 -0300

    Adicionando propriedades na classe Jogo

--- app/src/main/java/com/example/jogovelha/Jogo.java
+++ app/src/main/java/com/example/jogovelha/Jogo.java
@@ -6,6 +6,21 @@ public class Jogo implements Serializable {
 
     private String nomeJogador1;
     private String nomeJogador2;
+    private String jogadorAtual;
+    private String nomeDoVencedor;
+    private String marcacao;
+    private boolean houveGanhador;
+    private int qtdeJogadasDisponiveis;
+
+    public Jogo() {
+        this.houveGanhador = false;
+        this.qtdeJogadasDisponiveis = 9;
+        this.marcacao = "X";
+        this.nomeDoVencedor = "";
+        this.nomeJogador1 = "";
+        this.nomeJogador2 = "";
+        this.jogadorAtual = "";
+    }
 
     public String getNomeJogador1() {
         return nomeJogador1;
@@ -21,5 +36,58 @@ public class Jogo implements Serializable {
 
     public void setNomeJogador2(String nomeJogador2) {
         this.nomeJogador2 = nomeJogador2;
+    }
+
+    public String getJogadorAtual() {
+        return jogadorAtual;
+    }
+
+    public void setJogadorAtual(String jogadorAtual) {
+        this.jogadorAtual = jogadorAtual;
+    }
+
+    public String getNomeDoVencedor() {
+        return nomeDoVencedor;
+    }
+
+    public void setNomeDoVencedor(String nomeDoVencedor) {
+        this.nomeDoVencedor = nomeDoVencedor;
+    }
+
+    public String getMarcacao() {
+        return marcacao;
+    }
+
+    public void setMarcacao(String marcacao) {
+        this.marcacao = marcacao;
+    }
+
+    public boolean isHouveGanhador() {
+        return houveGanhador;
+    }
+
+    public void setHouveGanhador(boolean houveGanhador) {
+        this.houveGanhador = houveGanhador;
+    }
+
+    public int getQtdeJogadasDisponiveis() {
+        return qtdeJogadasDisponiveis;
+    }
+
+    public void setQtdeJogadasDisponiveis(int qtdeJogadasDisponiveis) {
+        this.qtdeJogadasDisponiveis = qtdeJogadasDisponiveis;
+    }
+
+    public void alterarMarcacao() {
+        if (this.marcacao.equals("X")) this.marcacao = "O";
+        else this.marcacao = "X";
+    }
+
+    public void alterarJogador() {
+        if (this.jogadorAtual.equals(this.nomeJogador1)) {
+            this.jogadorAtual = this.nomeJogador2;
+        } else {
+            this.jogadorAtual = this.nomeJogador1;
+        }
     }
 }
--- app/src/main/java/com/example/jogovelha/JogoActivity.java
+++ app/src/main/java/com/example/jogovelha/JogoActivity.java
@@ -4,18 +4,72 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class JogoActivity extends AppCompatActivity {
+public class JogoActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public Button button0, button1, button2, button3, button4;
+    private Button button5, button6, button7, button8;
+    private Jogo jogo;
+    private TextView textViewJogadorAtual;
+    private String tabuleiro[][] = {
+            {"_", "_", "_"},
+            {"_", "_", "_"},
+            {"_", "_", "_"}
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jogo);
 
-        Intent telaAnterior = getIntent();
-        Jogo jogo = (Jogo) telaAnterior.getSerializableExtra("jogo");
+        Som.parar();
+        Som.executar(this, R.raw.sinister);
+
+        button0 = findViewById(R.id.button0);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button4 = findViewById(R.id.button4);
+        button5 = findViewById(R.id.button5);
+        button6 = findViewById(R.id.button6);
+        button7 = findViewById(R.id.button7);
+        button8 = findViewById(R.id.button8);
+
+        button0.setOnClickListener(this);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+        button5.setOnClickListener(this);
+        button6.setOnClickListener(this);
+        button7.setOnClickListener(this);
+        button8.setOnClickListener(this);
+
+        textViewJogadorAtual = findViewById(R.id.textViewJogadorAtual);
+
+        setarJogadores();
+    }
+
+    private void setarJogadores() {
+        Intent telaJogador2 = getIntent();
+        jogo = (Jogo) telaJogador2.getSerializableExtra("jogo");
+
+        TextView textViewJogadores = findViewById(R.id.textViewJogadores);
+        String jogadores = jogo.getNomeJogador1()
+                .concat(" VS ")
+                .concat(jogo.getNomeJogador2());
+        textViewJogadores.setText(jogadores);
+
+        String jogadorAtual = "Jogando ".concat(jogo.getNomeJogador1());
+        textViewJogadorAtual.setText(jogadorAtual);
+    }
+
+    @Override
+    public void onClick(View v) {
 
     }
 }
